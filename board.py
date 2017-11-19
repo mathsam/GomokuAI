083bca838ae2a4dd369379dba4c6cf1f7ac075f7
mathsam <phychai.junyi@gmail.com>
2017-11-19 16:59:56 -0500

    Optimized data structure in Board so that simulation is much much faster

--- board.py
+++ board.py
@@ -1,7 +1,11 @@
+import copy
+import itertools
+
+import matplotlib.pyplot as plt
 import numpy as np
 
 EMPTY = 0
-PLAYER_A = 1
+PLAYER_A = 1  # black stone, first player to move
 PLAYER_B = 2
 
 TIE = 3
@@ -9,34 +13,71 @@ TIE = 3
 
 class Board(object):
 
-    def __init__(self, num_rows, num_cols, current_player=PLAYER_A, initial_state=None):
+    num_rows = 9
+    num_cols = 9
+
+    def __init__(self, initial_state=None):
         """
         Initialized a 2d board with shape (num_rows, num_cols)
-        :param num_rows: number of rows
-        :param num_cols: number of columns
-        :param current_player: first player to start the game
-        :param initial_state: _board may not necessarily start empty but from a previous state. Depending on the type,
-            _board can be initialized in the following ways:
-            str: state serialized by the method _serialize
-            list|tuple: list of positions in the format [(player, row, col), ...]
+        :param initial_state: None|list, _board may not necessarily start empty but from a previous state.
         """
-        self._board = [EMPTY] * (num_rows * num_cols)
-        self.num_rows = num_rows
-        self.num_cols = num_cols
-        self._current_player = current_player
-        if isinstance(initial_state, str):
-            self._current_player, self._board = self._deserialize(initial_state)
-        elif isinstance(initial_state, (list, tuple)):
-            for p, i, j in initial_state:
-                self[i,j] = p
+        if isinstance(initial_state, Board):
+            self = copy.deepcopy(initial_state)
+            return
+        else:
+            self._board = [EMPTY] * (Board.num_rows * Board.num_cols)
+        self._empty_indices = list(itertools.product(range(Board.num_rows), range(Board.num_cols)))
+        self.num_stones = 0
+        self.last_move = None
+        self.last_player = None
 
-    def update_state(self, new_state):
-        self._current_player, self._board = self._deserialize(new_state)
+    def update_state(self, move):
+        """Add a stone to the board. The color of the stone is automatically determined
+        :param move: tuple, (row_idx, col_idx)
+        :return: None
+        """
+        if self.num_stones % 2 == 0:
+            self[move] = PLAYER_A
+        else:
+            self[move] = PLAYER_B
+        self.last_move = move
+        self.last_player = self.current_player()
+        self.num_stones += 1
+        self._empty_indices.remove(move)
 
     def convert_into_2d_array(self):
         """Convert the board into 2d numpy array"""
         return np.array([self._board[i*self.num_cols:(i+1)*self.num_cols] for i in range(self.num_rows)],
                         dtype=np.int8)
+
+    def current_player(self, state=None):
+        """Return current player stored in this Board instance or decoded from input `state`
+        :param state: None|str
+        """
+        if self.num_stones % 2 == 0:
+            return PLAYER_A
+        return PLAYER_B
+
+    def avial_moves(self):
+        return self._empty_indices
+
+    def draw(self, ax=None):
+        if ax is None:
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+        ax.set_xticks(range(1, Board.num_cols+1))
+        ax.set_yticks(range(1, Board.num_rows+1))
+        ax.grid('on')
+        ax.set_xlim([0, Board.num_cols+1])
+        ax.set_ylim([0, Board.num_rows+1])
+        ax.set_aspect('equal')
+        for i in range(Board.num_rows):
+            for j in range(Board.num_cols):
+                if self[i, j] == PLAYER_A:
+                    ax.scatter(i+1, j+1, c='k', s=50)
+                elif self[i, j] == PLAYER_B:
+                    ax.scatter(i+1, j+1, c='r', s=50)
+
 
     def __getitem__(self, item):
         i, j = item
@@ -47,34 +88,6 @@ class Board(object):
         i, j = key
         idx = i*self.num_cols + j
         self._board[idx] = value
-
-    def _serialize(self):
-        """Return a string that contains the state information including current player and _board"""
-        return str((self._current_player, self._board))
-
-    def _serialize_potential_move(self, potential_move, potential_player):
-        i, j = potential_move
-        origs = self[i, j], self._current_player
-        self[i, j] = self._current_player = potential_player
-        str_repr = self._serialize()
-        self[i, j], self._current_player = origs
-        return str_repr
-
-    def _deserialize(self, state):
-        return eval(state)
-
-    def current_player(self, state=None):
-        """Return current player stored in this Board instance or decoded from input `state`
-        :param state: None|str
-        """
-        if state:
-            if state[1] == str(PLAYER_A):
-                return PLAYER_A
-            elif state[1] == str(PLAYER_B):
-                return PLAYER_B
-            else:
-                raise ValueError('Cannot decode input state')
-        return self._current_player
 
     def __str__(self):
         s = ''
--- gomoku_board.py
+++ gomoku_board.py
@@ -1,168 +1,74 @@
-import board
+from board import Board
+from board import PLAYER_A, PLAYER_B, TIE
 
 
+class GomokuBoard(Board):
 
-class GomokuBoard(board.Board):
+    search_directions = (
+        (0, 1), (1, 0), (1, 1), (1, -1),
+    )
 
-    search_directions = None
-
-    def __init__(self, num_rows, num_cols, current_player=board.PLAYER_A, initial_state=None):
-        board.Board.__init__(self, num_rows, num_cols, current_player, initial_state)
-        if GomokuBoard.search_directions is None:
-            GomokuBoard.search_directions = (
-                (zip(range(num_rows), [0]*num_rows), (0, 1)),
-                (zip([0]*num_cols, range(num_cols)), (1, 0)),
-                (zip([0]*(num_cols-4), range(num_cols-4)), (1, 1)),
-                (zip(range(num_rows-4), [0]*(num_rows-4)), (1, 1)),
-                (zip([0]*(num_cols-4), range(4, num_cols)), (-1, -1)),
-                (zip(range(4, num_rows), [0]*(num_rows-4)), (-1, -1))
-            )
-        self.search_directions = GomokuBoard.search_directions
-
-    def next_states(self, state=None):
-        pass
+    def __init__(self, initial_state=None):
+        Board.__init__(self, initial_state=None)
 
     def judge(self, state=None):
-        """
+        """Only need to search if the last move forms a five in a line
         :param state: None(default)|str(serialized state)
         :return: Return the player that wins if exists, otherwise return None
         """
-        if state:
-            curr_state = GomokuBoard(self.num_rows, self.num_cols, None, state)
-        else:
-            curr_state = self
-
-        if board.EMPTY not in curr_state._board:
-            return board.TIE
-
-        for start_pos, dir in self.search_directions:
-            for i, j in start_pos:
-                max_streak = 0
-                curr_stone_color = board.EMPTY
-                while 0 <= i < curr_state.num_rows and 0 <= j < curr_state.num_cols:
-                    if curr_state[i, j] is board.EMPTY:
-                        curr_stone_color = board.EMPTY
-                        max_streak = 0
-                    elif curr_stone_color is board.EMPTY:
-                        curr_stone_color = curr_state[i, j]
-                        max_streak = 1
-                    elif curr_state[i, j] == curr_stone_color:
-                        max_streak += 1
+        if self.num_stones == Board.num_rows*Board.num_cols:
+            return TIE
+        last_color = self.last_player
+        for dir in GomokuBoard.search_directions:
+            max_stone_in_line = 1
+            for sign in [-1, 1]:
+                for offset in xrange(1, 5):
+                    i = self.last_move[0] + dir[0]*offset*sign
+                    j = self.last_move[1] + dir[1]*offset*sign
+                    if 0 <= i < Board.num_rows and 0 <= j < Board.num_cols and self[i, j] == last_color:
+                        max_stone_in_line += 1
                     else:
-                        max_streak = 1
-                        curr_stone_color = curr_state[i, j]
-                    if max_streak == 5:
-                        return curr_stone_color
-                    i += dir[0]
-                    j += dir[1]
+                        break
+            if max_stone_in_line >= 5:
+                return self.last_player
         return None
-
-    def _search_ending_patterns(self, type, curr_state, stone_color):
-        if type == 'w': # must win pattern
-            patterns = [
-                ((board.EMPTY, stone_color, stone_color, stone_color, stone_color), (0,)),
-                ((stone_color, stone_color, stone_color, stone_color, board.EMPTY), (4,)),
-            ]
-        elif type == 'd' or type == 'a': # must defense/attack pattern
-            patterns = [
-                ((board.EMPTY, stone_color, stone_color, stone_color, stone_color), (0,)),
-                ((stone_color, stone_color, stone_color, stone_color, board.EMPTY), (4,)),
-                ((board.EMPTY, stone_color, stone_color, stone_color, board.EMPTY), (0, 4)),
-            ]
-        else:
-            raise ValueError('type of pattern not recognized')
-
-        for start_pos, dir in self.search_directions:
-            for i, j in start_pos:
-                while i < curr_state.num_rows and j < curr_state.num_cols:
-                    for p, mv in patterns:
-                        matched = True
-                        for offset in range(len(p)):
-                            x, y = i+dir[0]*offset, j+dir[1]*offset
-                            if x >= self.num_rows or y >= self.num_cols or x < 0 or y < 0:
-                                matched = False
-                                break
-                            if curr_state[x, y] != p[offset]:
-                                matched = False
-                                break
-                        if matched:
-                            return [(i+offset*dir[0], j+offset*dir[1]) for offset in mv]
-                    i += 1
-                    j += 1
-        return None
-
-    def next_states(self, state=None, next_player=None):
-        """"""
-        if state:
-            curr_state = GomokuBoard(self.num_rows, self.num_cols, None, state)
-        else:
-            curr_state = self
-        if next_player is None:
-            prev_player = curr_state.current_player()
-            if prev_player is board.PLAYER_A:
-                next_player = board.PLAYER_B
-            else:
-                next_player = board.PLAYER_A
-        if next_player is board.PLAYER_A:
-            other_player = board.PLAYER_B
-        else:
-            other_player = board.PLAYER_A
-
-        possible_moves = None
-        possible_moves = self._search_ending_patterns('w', curr_state, next_player)
-        if possible_moves is None:
-            possible_moves = self._search_ending_patterns('d', curr_state, other_player)
-        if possible_moves is None:
-            possible_moves = self._search_ending_patterns('a', curr_state, next_player)
-        if possible_moves is None:
-            possible_moves = set() # to store element (idx_row, idx_col)
-            for i in range(curr_state.num_rows):
-                for j in range(curr_state.num_cols):
-                    if curr_state[i, j] is not board.EMPTY:
-                        for offset_i in range(-1, 2):
-                            for offset_j in range(-1, 2):
-                                pos_i = i + offset_i
-                                pos_j = j + offset_j
-                                if (0 <= pos_i < curr_state.num_rows) and (0 <= pos_j < curr_state.num_cols):
-                                    if curr_state[pos_i, pos_j] is board.EMPTY:
-                                        possible_moves.add((pos_i, pos_j))
-
-        states = []
-        for p in possible_moves:
-            states.append(curr_state._serialize_potential_move(p, next_player))
-        return states
 
 
 if __name__ == '__main__':
     import random
     import matplotlib.pyplot as plt
-    grid_size = 15
-    ticks = range(15)
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.set_xticks(ticks)
-    ax.set_yticks(ticks)
-    ax.grid('on')
-    g_board = GomokuBoard(grid_size, grid_size, board.PLAYER_A,
-                          [(board.PLAYER_A,7,7)])
+    grid_size = 9
+    g_board = GomokuBoard()
+    g_board.update_state((grid_size//2, grid_size//2))
     winner = g_board.judge()
-    g_next = g_board._serialize()
-
-    g_next = random.choice(g_board.next_states(g_next))
 
     while not winner:
-        g_next = random.choice(g_board.next_states(g_next))
-        winner = g_board.judge(g_next)
-        print GomokuBoard(grid_size, grid_size, None, g_next)
-        print winner
-        ax.imshow(GomokuBoard(grid_size, grid_size, None, g_next).convert_into_2d_array())
-        plt.pause(0.25)
-    plt.pause(10)
+        next_move = random.choice(g_board.avial_moves())
+        g_board.update_state(next_move)
+        winner = g_board.judge()
+        #print g_board
+        #g_board.draw(ax)
+        #plt.pause(0.25)
+        #plt.clf()
 
-    array2d = GomokuBoard(grid_size, grid_size, None, g_next).convert_into_2d_array()
+    if winner == PLAYER_A:
+        print 'Winner is PLAYER A'
+    elif winner == PLAYER_B:
+        print 'Winner is PLAYER B'
+    else:
+        print 'Match TIE'
+    print g_board
+    g_board.draw()
+
+    plt.figure()
+    plt.axis('equal')
+    plt.grid('on')
+    plt.xlim([0, 10])
+    plt.xlim([0, 10])
+    array2d = g_board.convert_into_2d_array()
     for i in range(grid_size):
         for j in range(grid_size):
             if array2d[i,j] == 1:
-                plt.scatter(i, j, c='r')
+                plt.scatter(i+1, j+1, c='r', s=30)
             elif array2d[i,j] == 2:
-                plt.scatter(i, j, c='g')
+                plt.scatter(i+1, j+1, c='g', s=30)
